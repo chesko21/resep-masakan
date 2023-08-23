7a1985265c8a242806cd93e8b3d4b5d838b82da4
chesko21 <57135504+chesko21@users.noreply.github.com>
2023-08-24 00:19:02 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; 
 import './styles/tailwind.css';
 import App from './App';
 import './services/firebase';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(
+createRoot(rootElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
